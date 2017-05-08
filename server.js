528eaa41a0b7e323ba738c6896d042ab2b469254
Dawn Toh <dawn.toh@gmail.com>
2017-05-08 18:25:08 +0800

    Refactored middlware

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = 3000; //saying that the value should be constant
-
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-      console.log('Private route hit.');
-      next();
-    },
-    logger: function(req, res, next) {
-      var date = new Date().toString();
-      console.log('Request on ' + date + ': ' + req.method + ' ' + req.originalUrl);
-      next();
-    }
-};
+var middleware = require('./middleware.js');
 
 //Calling middleware at app level
 app.use(middleware.logger);
